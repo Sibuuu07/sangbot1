70fa2d2f40a86f184f36e1fbdaa6b8ab7700c9ee
secretgithuub <95080233+secretgithuub@users.noreply.github.com>
2021-12-18 15:07:22 +0530

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -12,5 +12,6 @@ let handler = async(m, { conn, text }) => {
 handler.help = ['bug/report'].map(v => v + ' <issue>')
 handler.tags = ['info']
 handler.command = /^(bug|report)$/i
-
+handler.owner = false
+handler.mods = true
 module.exports = handler
