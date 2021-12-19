32efcb05b38a376fd354ff0d4fab730ad2eb46e8
secretgithuub <95080233+secretgithuub@users.noreply.github.com>
2021-12-19 12:44:14 +0530

    Update tokio.js

--- plugins/tokio.js
+++ plugins/tokio.js
@@ -2,7 +2,7 @@ let handler = async m => m.reply(`
 ☕ *SangBot :* SangBot is AI whatsapp robot\n\n🔗 *URL :* http://instagram
 com/yugesh.the_techie
 `.trim()) // repository
-handler.help = ['tokio']
+handler.help = ['Sangbot']
 handler.tags = ['info']
 handler.command = /^sangbot|repo$/i
 
