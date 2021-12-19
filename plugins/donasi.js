cd37370dcc9dc3594a930c055cf0db05bed4e8ff
secretgithuub <95080233+secretgithuub@users.noreply.github.com>
2021-12-19 08:41:18 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,6 @@
 let handler = async m => m.reply(`
 👽 Owner is rich, he doesn't need your money.
+Just Need Your Support on Instagram follow us on https://instagram.com/yugesh.the_techie
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donate']
 handler.tags = ['info']
