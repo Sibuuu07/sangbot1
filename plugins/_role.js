2fbf224ba211b1fbef759da19e99b545b9632097
secretgithuub <95080233+secretgithuub@users.noreply.github.com>
2021-12-19 10:36:01 +0530

    Update _role.js

--- plugins/_role.js
+++ plugins/_role.js
@@ -2,15 +2,16 @@ let handler = m => m
 
 handler.before = function (m) {
   let user = global.db.data.users[m.sender]
-        let role = (user.level <= 10) ? 'Beginner'
-          : ((user.level >= 10) && (user.level <= 20)) ? 'Kittel Town'
-          : ((user.level >= 20) && (user.level <= 30)) ? 'Black Woods'
-          : ((user.level >= 30) && (user.level <= 40)) ? 'Farmount'
-          : ((user.level >= 40) && (user.level <= 50)) ? 'Rosenvale'
-          : ((user.level >= 50) && (user.level <= 60)) ? 'Amberhill'
-          : ((user.level >= 60) && (user.level <= 70)) ? 'Master Fap'
-          : ((user.level >= 70) && (user.level <= 80)) ? 'Master Woods'
-          : 'Legend'
+        let role = (user.level <= 10) ? 'Baby 🐣'
+          : ((user.level >= 10) && (user.level <= 20)) ? 'Kids 🚸'
+          : ((user.level >= 20) && (user.level <= 30)) ? 'Human💜'
+          : ((user.level >= 30) && (user.level <= 40)) ? 'Alien'
+          : ((user.level >= 40) && (user.level <= 50)) ? 'Master'
+          : ((user.level >= 50) && (user.level <= 60)) ? 'Grand-Master'
+          : ((user.level >= 60) && (user.level <= 70)) ? 'Superhero 🦸‍♂️'
+          : ((user.level >= 70) && (user.level <= 80)) ? 'Mastered Gandv3'
+          : ((user.level >= 80) && (user.level <= 100)) ? 'God'
+          : 'Management Team Member'
   user.role = role
   return true
 }
