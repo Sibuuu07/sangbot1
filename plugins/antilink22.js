0d43013ebefce4f02201b80f87c5603e28d1cd67
secretgithuub <95080233+secretgithuub@users.noreply.github.com>
2021-12-31 18:13:19 +0530

    Add files via upload

---
+++ plugins/antilink22.js
@@ -0,0 +1,32 @@
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async function(m, { conn , args, text, isAdmin, isBotAdmin, groupMetadata }) {
+
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let asu = m.sender
+  let vir = asu.split("@s.whatsapp.net")[0]
+  if (!m.isGroup) { 
+    await m.reply('_Dont Send Links !?!?_')
+    conn.blockUser(m.sender, "add")
+  } else {
+    await conn.reply(m.chat, `
+*[ ANTI LINK ]*
+_Detected *@${asu.split("@")[0]}* send Link!_
+Sorry you will be kicked by *BY_ERROR*
+`.trim(), m, { 
+      contextInfo: {  
+        mentionedJid: [asu]
+      }
+    })
+ if (isBotAdmin) {
+   conn.groupRemove(m.chat, [asu])
+     } else { 
+     	m.reply('```MAKE BOT_ERROR AS ADMIN !!!```')
+    }
+  } conn.sendMessage(vir + '@s.whatsapp.net', `${m.text}`, MessageType.text)
+}
+handler.customPrefix = /https|youtu.be|https|chat.whatsapp.com|https|vt.tiktok.com|https/i
+handler.command = new RegExp
+
+handler.fail = null
+
+module.exports = handler
